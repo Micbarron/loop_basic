1ecaf1002b0a36d7b5b765916dca3d1bc19a3bd7
Michael Barron <michael.barron08@gmail.com>
2021-07-06 14:06:20 -0500

    final commit

---
+++ loop_basic.py
@@ -0,0 +1,40 @@
+# Prob 1 print integers from 1 - 150
+
+# for x in range (0,101):
+#     print(x)
+
+# Prob 2print multiples of 5 from 5 to 1000
+
+# for x in range (5,1005, 5):
+#     print(x)
+
+# Prob 3 print integers 1 - 100 if divisible by 5, print "Coding" instead. If divisble by 10, print "Coding Dojo"
+
+# for x in range(0,101):
+#     if x % 10 == 0:
+#         print("CodingDojo")
+#     elif x % 5 == 0:
+#         print("Coding")
+#     else:
+#         print(x)
+
+# Prob 4 add odd integers from 0 to 500000, and print the final sum
+
+# count = 0
+# for x in range(0,500001):
+#     if x % 2 == 1:
+#         count += x
+# print(count)
+
+# Prob 5 print positive numbers starting at 2018, counting down by fours.
+
+# for x in range(2018, 0, -4):
+#     print(x)
+
+# Prob 6 Set three variables: lowNum, highNum, mult. Starting at lowNum and going through highNum, print only the integers that are a multiple of mult. For example, if lowNum=2, highNum=9, and mult=3, the loop should print 3, 6, 9 (on successive lines)
+
+# lowNum = 4
+# highNum = 500
+# mult = 6
+# for x in range(lowNum, highNum, mult):
+#     print(x)
